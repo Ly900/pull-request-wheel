2bb4c64b2f265b13f26c4692c6aa914d9227fdb8
Ly Nguyen <lynguyendc@gmail.com>
2026-05-15 22:38:46 -0400

    Add skull icon to title; improve dark mode styling and WCAG contrast

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <>
-      <h1>Pull Request Wheel of Death</h1>
+      <h1><span style={{ fontSize: '1em', verticalAlign: 'top' }}>💀</span> Pull Request Wheel of Death</h1>
       <p>Not sure who to assign the pull request to? Fret no more. Avoid those glares you get when you assign to the same dev over and over again. Blame it on the Wheel of Death!</p>
 
       <div className="layout">
